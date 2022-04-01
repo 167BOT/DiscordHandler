c4a68f0dcfd1733b84d7e71f0bd6ec2e02d79302
167_dev <74829313+167BOT@users.noreply.github.com>
2022-04-01 22:21:49 +0200

    Delete events directory

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  run: (client) => {
-    client.on("guildMemberAdd", (member) => {
-      console.log(`Hola, ${member.user.username}. Tu ID es: ${member.id}`)
-      /*code*/
-    });
-  }
-}
--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  run: (client) => {
-    client.on("ready", () => {
-      console.log(`I'm ready to give services`)
-    });
-
-    client.login("OTU0OTAzNTg3NDIxNTczMTgx.YjZ5RA.37LoCTir-_9JTymCYKMNTHhtVOQ")
-  }
-}
